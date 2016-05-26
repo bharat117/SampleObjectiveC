51837322b8c5ba8db2835bf9108c43f2334b11b7
Bharat Byan <bharat.byan@mobileprogramming.net>
2016-05-26 23:28:46 +0530

    importing code to github

---
+++ objectiveSample/objectiveSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objectiveSample
+//
+//  Created by Bharat Byan on 5/26/16.
+//  Copyright © 2016 Bharat Byan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objectiveSample/objectiveSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objectiveSample
+//
+//  Created by Bharat Byan on 5/26/16.
+//  Copyright © 2016 Bharat Byan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
